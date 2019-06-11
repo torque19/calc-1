d68943548b295f7894d0705ca4a4bf6df7820f34
Shobha Tyagi <tyagishobha@gmail.com>
2019-06-11 23:43:14 +0530

    Update calculator.c

--- calculator.c
+++ calculator.c
@@ -2,18 +2,20 @@
 #include<conio.h>
 void main()
 {
-double a, b, result, choice;
+double a, b, result;
+int choice;
 clrscr();
 result = 0;
 printf("Enter two nubers");
-scanf("%d%d", &a,&b);
+scanf("%lf%lf", &a,&b);
 printf("Enter your choice: \n 1 for adition\n 2 for subtraction\n 3 for multiplication");
+scanf("%d", &choice);
 switch(choice){
 case 1: result = a+b;
-	printf("The sum of %d and %d is: %d",a,b,result);
+	printf("The sum of %lf and %lf is: %lf",a,b,result);
 	break;
 case 2: result = a-b; 
-	printf("The difference of %d and %d is: %d",a,b,result);
+	printf("The difference of %lf and %lf is: %lf",a,b,result);
 	break;
 case 3: break;
 default:"invalid choice";
